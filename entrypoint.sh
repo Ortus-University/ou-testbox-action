66d910916f00f8cc32cc4e87ed38dda3d3830984
BalbinoOrtus <80276017+BalbinoOrtus@users.noreply.github.com>
2021-05-07 16:34:41 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ box install
 box server start cfengine=$1
 box testbox run outputFile=testbox.xml reporter=junit
 echo "done1"
-cat /github/workspace/testbox.xml
-box testbox run outputformats=json,simple reporter=Console
+box testbox run outputformats=json,simple reporter=Text
 echo "done2"
-cat /github/workspace/testbox.xml
-box server log
+echo "doneFinal"
