959cacdd3ed2856a22351d83766f27b55dcae1af
BalbinoOrtus <80276017+BalbinoOrtus@users.noreply.github.com>
2021-05-07 13:00:52 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "Starting the testing script"
 
 echo "Your CFML engine of choice is $1"
 
-box install production=false
+box install --verbose production=false
 box server start cfengine=$1 --trace
-box testbox run outputFile=testbox.xml reporter=junit 
+box testbox run outputFile=testbox.xml reporter=junit
+Ls -lar
