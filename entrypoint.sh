194873f7e262cc7fd05a39d73384a6df6281bd37
BalbinoOrtus <80276017+BalbinoOrtus@users.noreply.github.com>
2021-05-07 16:11:26 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ echo "Starting the testing script"
 
 echo "Your CFML engine of choice is $1"
 
-box install --verbose
+box install
 box server start cfengine=$1
 box testbox run outputFile=testbox.xml reporter=junit
 echo "done"
 box testbox run outputFile=testbox.xml reporter=junit
 echo "done again"
-echo "$(</github/workspace/testbox.xml)" 
+echo "what is this" 
 box server log
