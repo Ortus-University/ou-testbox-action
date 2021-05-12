f00e2f9d93b5abc24f7b2d69291095c1c219dc25
BalbinoOrtus <80276017+BalbinoOrtus@users.noreply.github.com>
2021-05-12 11:46:18 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,10 @@ echo "Starting the testing script"
 echo "Your CFML engine of choice is $1"
 
 box install
+box server set web.http.port=8080
+box package set testbox.runner="http://localhost:8080/tests/runner.cfm"
 box server start cfengine=$1
 box testbox run outputformats=json,simple reporter=json
 echo "done1"
-box !curl http://127.0.0.1:44427/tests/runner.cfm?&recurse=true&reporter=json&verbose=false
+box !curl http://127.0.0.1:8080/tests/runner.cfm?&recurse=true&reporter=json&verbose=false
 echo "doneFinal"
