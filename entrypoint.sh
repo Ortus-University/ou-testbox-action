9c0a07c06cb553af474b6d23236a8053ff6286d4
BalbinoOrtus <80276017+BalbinoOrtus@users.noreply.github.com>
2021-05-14 14:21:18 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,5 @@ box server set web.http.port=8080
 box package set testbox.runner="http://localhost:8080/tests/runner.cfm"
 box server start cfengine=$1
 box testbox run outputformats=json,simple reporter=JSON
-echo "done1"
-curl http://127.0.0.1:8080/tests/runner.cfm?&recurse=true&reporter=JSON&verbose=true
 echo "doneFinal"
 box echo ${exitCode}
